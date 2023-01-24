8b22e1e476819a0ccf9c87cf2c40ed839b469216
Victor Teka <tekav@letshego.com>
2023-01-24 20:44:25 +0300

    Initial Commit

---
+++ NewsAppSwift/NewsAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppSwiftApp.swift
+//  NewsAppSwift
+//
+//  Created by Victor Teka on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
